42ad79b91cf7324234e4e5dda4b16cbfa2a026ad
Bala Krishna <86514883+Xorizn@users.noreply.github.com>
2023-04-24 22:29:31 +0800

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -4,6 +4,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json({ developer: '@Xorizn', mess: 'Take a look at this readme link https://github.com/Xorizn/xorizn-search#readme' })
+    res.redirect('https://github.com/Xorizn/xorizn-search#readme')
 })
 module.exports = router
